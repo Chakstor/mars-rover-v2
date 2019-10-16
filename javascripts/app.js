c3bc479dbe711679e176255b5b6a65d6982c178b
Carlos Mateo <cmc2k5@gmail.com>
2019-10-16 15:15:33 +0200

    Bunch of new things + first GUI try

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,125 +1,169 @@
-// Rover Object Goes Here
-// ======================
+$(document).ready(function () {
+    // Variables
+    // ======================
 
-var rover = {
-    direction: 'N',
-    travelLog: ['[0, 0]'],
-    x: 0,
-    y: 0
-};
+    var rover = {
+        direction: 'N',
+        travelLog: ['[0, 0]'],
+        x: 0,
+        y: 0
+    };
+
+    var gridDefinition = {
+        rows: 3,
+        columns: 3
+    };
+
+    var grid = [];
 
 
-// Grid Goes Here
-// ======================
+    // Generate Grid
+    // ======================
 
-var grid = {
-    rows: 10,
-    columns: 10
-};
+    for (var row = 0; row <= gridDefinition.rows - 1; row++) {
+        var arr = [];
+        $('#grid').append('<ul></ul>');
 
+        for (var col = 0; col <= gridDefinition.columns - 1; col++) {
+            $('#grid ul').eq(row).append(`<li></li>`)
+        }
 
-// Methods Go Here
-// ======================
-
-function turnLeft(rover) {
-    var previousDirection = rover.direction;
-    updateDirection(rover.direction, 'left');
-    printDirectionStatus(previousDirection, rover.direction, 'turnLeft');
-}
-
-function turnRight(rover) {
-    var previousDirection = rover.direction;
-    updateDirection(rover.direction, 'right');
-    printDirectionStatus(previousDirection, rover.direction, 'turnRight');
-}
-
-function moveForward(rover) {
-    switch(rover.direction) {
-        case 'N':
-            rover.y = (rover.y - 1 >= 0) ? rover.y - 1 : rover.y;
-            break;
-        case 'E':
-            rover.x = (rover.x + 1 < grid.columns) ? rover.x + 1 : rover.x;
-            break;
-        case 'S':
-            rover.y = (rover.y + 1 < grid.rows) ? rover.y + 1 : rover.y;
-            break;
-        case 'W':
-            rover.x = (rover.x - 1 >- 0) ? rover.x - 1 : rover.x;
-            break;
+        grid.push(arr);
     }
 
-    rover.travelLog.push(`[${rover.x}, ${rover.y}]`);
-    printPositionStatus(rover, 'moveForward');
-}
+    updateRoverOnMap();
 
-function moveBackward(rover) {
-    switch (rover.direction) {
-        case 'N':
-            rover.y = (rover.y + 1 < grid.rows) ? rover.y + 1 : rover.y;
-            break;
-        case 'E':
-            rover.x = (rover.x - 1 > 0) ? rover.x - 1 : rover.x;
-            break;
-        case 'S':
-            rover.y = (rover.y - 1 >= 0) ? rover.y - 1 : rover.y;
-            break;
-        case 'W':
-            rover.x = (rover.x + 1 < grid.columns) ? rover.x + 1 : rover.x;
-            break;
+
+    // Methods
+    // ======================
+
+    function turnLeft(rover) {
+        var previousDirection = rover.direction;
+        updateDirection(rover.direction, 'left');
+        printDirectionStatus(previousDirection, rover.direction, 'turnLeft');
     }
 
-    rover.travelLog.push(`[${rover.x}, ${rover.y}]`);
-    printPositionStatus(rover, 'moveBackward');
-}
-
-function updateDirection(direction, turn) {
-    switch(direction) {
-        case 'N':
-            rover.direction = turn === 'left' ? 'W' : 'E';
-            break;
-        case 'E':
-            rover.direction = turn === 'left' ? 'N' : 'S';
-            break;
-        case 'S':
-            rover.direction = turn === 'left' ? 'E' : 'W';
-            break;
-        case 'W':
-            rover.direction = turn === 'left' ? 'S' : 'N';
-            break;
+    function turnRight(rover) {
+        var previousDirection = rover.direction;
+        updateDirection(rover.direction, 'right');
+        printDirectionStatus(previousDirection, rover.direction, 'turnRight');
     }
-}
 
-function roverCommands(commands) {
-    for (var i = 0; i <= commands.length; i++) {
-        switch(commands[i]) {
-            case 'f':
-                moveForward(rover);
+    function moveForward(rover) {
+        var previousPos = {
+            x: rover.x,
+            y: rover.y
+        }
+
+        switch(rover.direction) {
+            case 'N':
+                rover.x = (rover.x - 1 > - 0) ? rover.x - 1 : rover.x;
                 break;
-            case 'b':
-                moveBackward(rover);
+            case 'E':
+                rover.y = (rover.y + 1 < gridDefinition.columns) ? rover.y + 1 : rover.y;
                 break;
-            case 'r':
-                turnRight(rover);
+            case 'S':
+                rover.x = (rover.x + 1 < gridDefinition.rows) ? rover.x + 1 : rover.x;
                 break;
-            case 'l':
-                turnLeft(rover);
+            case 'W':
+                rover.y = (rover.y - 1 >= 0) ? rover.y - 1 : rover.y;
+                break;
+        }
+
+        updateRoverTravelLog(rover);
+        printRoverPosition(rover, 'moveForward');
+        updateGridMap(previousPos, rover);
+    }
+
+    function moveBackward(rover) {
+        var previousPos = {
+            x: rover.x,
+            y: rover.y
+        }
+
+        switch (rover.direction) {
+            case 'N':
+                rover.x = (rover.x + 1 < gridDefinition.rows) ? rover.x + 1 : rover.x;
+                break;
+            case 'E':
+                rover.y = (rover.y - 1 >= 0) ? rover.y - 1 : rover.y;
+                break;
+            case 'S':
+                rover.x = (rover.x - 1 > 0) ? rover.x - 1 : rover.x;
+                break;
+            case 'W':
+                rover.y = (rover.y + 1 < gridDefinition.columns) ? rover.y + 1 : rover.y;
+                break;
+        }
+
+        updateRoverTravelLog(rover);
+        printRoverPosition(rover, 'moveBackward');
+        updateGridMap(previousPos, rover);
+    }
+
+    function updateDirection(direction, turn) {
+        switch(direction) {
+            case 'N':
+                rover.direction = turn === 'left' ? 'W' : 'E';
+                break;
+            case 'E':
+                rover.direction = turn === 'left' ? 'N' : 'S';
+                break;
+            case 'S':
+                rover.direction = turn === 'left' ? 'E' : 'W';
+                break;
+            case 'W':
+                rover.direction = turn === 'left' ? 'S' : 'N';
                 break;
         }
     }
-}
 
-function printDirectionStatus(previousDirection, newDirection, calledMethod) {
-    console.log(`Previous direction: ${previousDirection}`);
-    console.log(`New direction: ${newDirection}`);
-    console.log(`${calledMethod} was called!`);
-}
+    function roverCommands(commands) {
+        for (var i = 0; i <= commands.length; i++) {
+            switch(commands[i]) {
+                case 'f':
+                    moveForward(rover);
+                    break;
+                case 'b':
+                    moveBackward(rover);
+                    break;
+                case 'r':
+                    turnRight(rover);
+                    break;
+                case 'l':
+                    turnLeft(rover);
+                    break;
+            }
+        }
+    }
 
-function printPositionStatus(rover, calledMethod) {
-    console.log(`${calledMethod} was called`);
-    console.log(`new position: [${rover.x}, ${rover.y}]`);
-}
+    function updateGridMap(previousPos, rover) {
+        grid[previousPos.x][previousPos.y] = null;
+        grid[rover.x][rover.y] = 'X';
 
-function getRoverTravelLog() {
-    console.log(rover.travelLog);
-}
+        console.log(grid);
+    }
+
+    function updateRoverOnMap() {
+        $('#grid ul:nth-child(' + rover.y + 1 + ') li:nth-child(' + rover.x + 1 + ')').text('x');
+    }
+
+    function printDirectionStatus(previousDirection, newDirection, calledMethod) {
+        console.log(`Previous direction: ${previousDirection}`);
+        console.log(`New direction: ${newDirection}`);
+        console.log(`${calledMethod} was called!`);
+    }
+
+    function printRoverPosition(rover, calledMethod) {
+        console.log(`${calledMethod} was called`);
+        console.log(`new position: [${rover.x}, ${rover.y}]`);
+    }
+
+    function updateRoverTravelLog(rover) {
+        rover.travelLog.push(`[${rover.x}, ${rover.y}]`);
+    }
+
+    function getRoverTravelLog() {
+        console.log(rover.travelLog);
+    }
+});
